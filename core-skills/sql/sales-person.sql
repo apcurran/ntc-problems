48f17c89ef903bfbeeec5299287351eb9283925a
Alex Curran <alexcurran.va@gmail.com>
2026-03-03 14:40:45 -0600

    NOT EXISTS alt subquery solution

--- core-skills/sql/sales-person.sql
+++ core-skills/sql/sales-person.sql
@@ -7,3 +7,16 @@ WHERE sales_person.sales_id NOT IN (
     INNER JOIN company ON orders.com_id = company.com_id
     WHERE company.name = 'CRIMSON'
 );
+
+
+-- solution 2 (NOT EXISTS alt subquery approach)
+SELECT sales_person.name
+FROM sales_person
+WHERE NOT EXISTS (
+    SELECT orders.sales_id
+    FROM orders
+    INNER JOIN company ON orders.com_id = company.com_id
+    WHERE
+        orders.sales_id = sales_person.sales_id AND
+        company.name = 'CRIMSON'
+);
